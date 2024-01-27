fa2be1fb0a79db59f18044bca8ba63742ea8ca1c
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-01-27 16:34:21 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function superExcuseGenerator(question) {
+    return question[Math.floor(Math.random() * question.length)];
+  }
+  let whoExcuse = superExcuseGenerator(who);
+  let actionExcuse = superExcuseGenerator(action);
+  let whatExcuse = superExcuseGenerator(what);
+  let whenExcuse = superExcuseGenerator(when);
+  let excuse =
+    whoExcuse +
+    " " +
+    actionExcuse +
+    " " +
+    whatExcuse +
+    " " +
+    whenExcuse +
+    " " +
+    ".";
+
+  document.getElementById("excuse").innerText = excuse;
 };
